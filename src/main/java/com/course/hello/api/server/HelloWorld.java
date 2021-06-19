3adb749b9e678bcccd1b1264f8bb0dca197061da
DangTruongSinh <nguyenthuanh01234@gmail.com>
2021-06-18 22:52:09 -0700

    Add log to project

--- src/main/java/com/course/hello/api/server/HelloWorld.java
+++ src/main/java/com/course/hello/api/server/HelloWorld.java
@@ -1,13 +1,18 @@
 package com.course.hello.api.server;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld {
 
+	private Logger log = LoggerFactory.getLogger(HelloWorld.class);
+
 	@GetMapping("/hello")
 	public String hello() {
+		log.info("Add log");
 		return "hello world asds";
 	}
 }
